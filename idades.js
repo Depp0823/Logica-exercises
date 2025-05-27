fb37e6d04a62de5a509c1dd94e7e99a778af36b1
Depp0823 <migs.22.18.03@gmail.com>
2025-05-27 17:09:02 -0300

    PRIMEIRA PROGRAMAÇÃO
    PERGUNTA SUA IDADE E DIZ SE VOCÊ É MAIOR OU MENOR DE IDADE

---
+++ idades.js
@@ -0,0 +1,15 @@
+//ler idade do usuário
+//se a idade for 18 ou mais, dizer que é maior de idade
+//se não, dizer que é menor de idade
+
+let idade = prompt("Por Favor informe sua idade:")
+
+if( idade >= 18 ){
+
+    alert("Você é maior de idade.")
+}
+
+else{
+
+    alert("Você é menor de idade.")
+}
